978d8d78a5b678825d3bd7e1bb04e2a4b8f07fe3
shaheer904 <54603049+shaheer904@users.noreply.github.com>
2021-05-20 12:35:55 +0500

    k

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.htmlgit'))
   })
 }
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
